e0dea94b2fd108c8cbdda6f18ba998d7c49087fe
Arash Nemat Zadeh <arashnm80@gmail.com>
2022-08-22 13:00:20 +0430

    remove duplicate readme texts from main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,3 @@
-// "/usr/include/python3.8/" should be added to includePath in c_cpp_properties.json
-// build with "g++ main.cpp -o app -I /usr/include/python3.8/ -lpython3.8"
-// <curses.h> is a better replacement for <conio.h>
-// a tutorial source: https://www.codeproject.com/Articles/820116/Embedding-Python-program-in-a-C-Cplusplus-code
-
 #include <stdio.h>
 #include <iostream>
 #include <curses.h>
